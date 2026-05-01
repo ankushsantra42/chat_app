679e95207baa0afc24a98d60d3e22d8da99b849f
ankushsantra42 <ankushsantra42@gmail.com>
2026-05-01 16:58:55 +0530

    change in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,13 +30,8 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
-  // });
-  app.get("/(.*)", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../../", "frontend", "dist", "index.html"),
-    );
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
 
