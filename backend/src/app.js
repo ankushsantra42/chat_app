f1bb0de13564a5905c5173dcc8e7aa531c2aa68b
ankushsantra42 <ankushsantra42@gmail.com>
2026-05-01 10:50:49 +0530

    package.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,4 +25,14 @@ const messageRouter = require("./routes/message.route");
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+  });
+}
+
 module.exports = app;
